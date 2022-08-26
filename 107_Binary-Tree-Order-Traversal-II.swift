54b28050c372bae1c2afbbc6a4d5a87b808592b3
Joechen <s900567@yahoo.com.tw>
2022-08-27 00:50:51 +0800

    [Add] leetcode Solution

---
+++ 107_Binary-Tree-Order-Traversal-II.swift
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        
+        guard root != nil else { return [] }
+        
+        var queue = [TreeNode]()
+        var vec = [Int]()
+        var result = [[Int]]()
+        
+        queue.append(root!)
+        
+        while !queue.isEmpty {
+            
+            let size = queue.count
+            
+            for _ in 0..<size {
+                let current = queue.removeFirst()
+                vec.append(current.val)
+                
+                if current.left != nil {
+                    queue.append(current.left!)
+                }
+                if current.right != nil {
+                    queue.append(current.right!)
+                }
+                
+            }
+            result.append(vec)
+            vec = [Int]()
+        }
+        
+        return result.reversed()
+    }
+}

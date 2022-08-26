649aefd85282fa0ac1950e5aa532dade948b4694
Joechen <s900567@yahoo.com.tw>
2022-08-26 14:03:31 +0800

    [Modify] leetcode solution

--- 283_Move-Zeroes.swift
+++ 283_Move-Zeroes.swift
@@ -1,30 +1,17 @@
 class Solution {
     func moveZeroes(_ nums: inout [Int]) {
         
-        var zeroIndex: Int = 0
+        var start: Int = 0
 
         for i in 0..<nums.count {
-
             if (nums[i] != 0) {
-
-                let temp = nums[i]
-                nums[i] = nums[zeroIndex]
-                nums[zeroIndex] = temp
-                zeroIndex += 1
+                nums[start] = nums[i]
+                start += 1
             }
         }
         
-        
-        /*var result: [Int] = Array(repeating: 0, count: nums.count)
-        var index: Int = 0
-        
-        for i in 0..<nums.count {
-            if (nums[i] != 0) {
-                result[index] = nums[i]
-                index += 1
-            }
+        for i in start..<nums.count {
+            nums[i] = 0
         }
-        
-        nums = result*/
     }
 }

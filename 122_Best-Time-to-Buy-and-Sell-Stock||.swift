87ebef55c78e1d8c8ba5a7d575d5e77aa2f22dc4
Joechen <s900567@yahoo.com.tw>
2022-09-01 21:54:46 +0800

    [Modify] 122 Stock || greedy solution

--- 122_Best-Time-to-Buy-and-Sell-Stock||.swift
+++ 122_Best-Time-to-Buy-and-Sell-Stock||.swift
@@ -1,18 +1,16 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
         
-        var noShare: Int = 0
-        var withShare: Int = -prices[0]
-
+        
+        // Greedy
+        var noStock = 0
+        var withStock = -prices[0]
+        
         for i in 0..<prices.count {
-            
-            let temp = noShare
-            // no share and sell today
-            noShare = max(noShare, withShare + prices[i])
-            // with share and buy today
-            withShare = max(withShare, temp - prices[i])
+            noStock = max(noStock, withStock + prices[i])
+            withStock = max(withStock, noStock - prices[i])
         }
-
-        return noShare    
+        
+        return noStock
     }
 }
